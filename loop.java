780fb838562513614c26aa5e4e14a57c82bb9531
Abhinav <pabhinav689@gmail.com>
2023-03-03 15:21:52 +0530

    loop.java

--- loop.java
+++ loop.java
@@ -1,4 +1,3 @@
-
 /*import java.util.Scanner;
 public class loop {
     public static void main(String[] args) {
@@ -9,26 +8,50 @@ public class loop {
         
     }
 }*/
+// import java.util.*;
+
+// public class loop {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int input;
+//         int marks;
+//         do {
+//             marks = sc.nextInt();
+//             if (marks <= 100 && marks >= 90) {
+//                 System.out.println("this is good");
+//             } else if (marks <= 89 && marks >= 60) {
+//                 System.out.println("this is also good");
+//             } else if (marks <= 59 && marks >= 0) {
+//                 System.out.println("this is good as well");
+//             } else {
+//                 System.out.println("invalid data");
+//             }
+//             System.out.println("want to continue yes(1) or no(0)");
+//             input = sc.nextInt();
+//         } while (input == 1);
+//     }
+// }
 import java.util.*;
 
 public class loop {
-    public static void main (String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int input;
-        int marks;
-        do {
-            marks = sc.nextInt();
-            if (marks<=100 && marks>=90) {
-                System.out.println("this is good");
-            } else if(marks<=89 && marks>=60) {
-                System.out.println("this is also good");
-            } else if(marks<=59 && marks>=0) {
-                System.out.println("this is good as well");
-            } else {
-                System.out.println("invalid data");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
             }
-            System.out.println("want to continue yes(1) or no(0)");
-            input = sc.nextInt();
-        } while(input == 1);
+        }
+        if (isPrime) {
+            if (n == 1) {
+                System.out.println("This is neither prime not composite");
+            } else {
+                System.out.println("This is a prime number");
+            }
+        } else {
+            System.out.println("This is not a prime number");
+        }
     }
 }
