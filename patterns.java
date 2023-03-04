a01f09e0c952adc0b768ead6741446379113c2c0
Abhinav <pabhinav689@gmail.com>
2023-03-05 00:06:25 +0530

    patterns.java

--- patterns.java
+++ patterns.java
@@ -7,18 +7,22 @@
 //         int n = 4;
 //         int m = 5;
 //         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < m; j++) {
+//             for (int j = 0; j < m; j++) {gcv9
 //                 System.out.print("*");
 //             }
 //             System.out.println();            
 //         }
 //     }
 // }
+//     print- *****
+//            *   *
+//            *   *
+//            *****
 import java.util.*;
 public class patterns {
 public static void main(String args[]) {
-int n = 4;
-int m = 5;
+int n = 5;
+int m = 4;
 for(int i=0; i<n; i++) {
 for(int j=0; j<m; j++) {
 if(i == 0 || i == n-1 || j == 0 || j == m-1) {
