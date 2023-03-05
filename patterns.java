3908413dab62cc7d0c9e090cf0d1abeac81d1633
Abhinav <pabhinav689@gmail.com>
2023-03-05 20:17:43 +0530

    patterns.java

--- patterns.java
+++ patterns.java
@@ -7,7 +7,7 @@
 //         int n = 4;
 //         int m = 5;
 //         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < m; j++) {gcv9
+//             for (int j = 0; j < m; j++) {
 //                 System.out.print("*");
 //             }
 //             System.out.println();            
@@ -18,20 +18,38 @@
 //            *   *
 //            *   *
 //            *****
+// import java.util.*;
+// public class patterns {
+//     public static void main(String[] args) {
+//         int n = 4;
+//         int m = 5;
+//         for ( int i = 1; i<=4; i++) {
+//             for( int j = 1; j<=5; j++) {
+//                 if( i==1 || j==1 || i==n || j==m) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             } System.out.println();
+//         } 
+//     }
+
+// }
+// print - 
+// *
+// **
+// ***
+// ****
 import java.util.*;
 public class patterns {
-public static void main(String args[]) {
-int n = 5;
-int m = 4;
-for(int i=0; i<n; i++) {
-for(int j=0; j<m; j++) {
-if(i == 0 || i == n-1 || j == 0 || j == m-1) {
-System.out.print("*");
-} else {
-System.out.print(" ");
-}
-}
-System.out.println();
-}
-}
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
 }
